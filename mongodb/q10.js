bae1d3bf4231a5c8524c5b5705e72490675c7d43
nairx <nairxprv@gmail.com>
2025-06-30 11:49:29 +0530

    updated

--- mongodb/q10.js
+++ mongodb/q10.js
@@ -7,3 +7,12 @@ mongod -replSet rs1 -logpath "d:\mongo-replica\data1\1.log" --dbpath "d:\mongo-r
 mongod -replSet rs1 -logpath "d:\mongo-replica\data2\2.log" --dbpath "d:\mongo-replica\data2" --port 27019
 
 mongod -replSet rs1 -logpath "d:\mongo-replica\data3\3.log" --dbpath "d:\mongo-replica\data3" --port 27020
+
+Follow these instructions to configure replica set:
+
+mongosh --port 27018
+
+rs.initiate({_id:"rs1",members:[{_id:0,host:"127.0.0.1:27018"},{_id:1,host:"127.0.0.1:27019"},{_id:2,host:"127.0.0.1:27020"}]})
+
+rs.config()  //to check the config
+rs.status()
