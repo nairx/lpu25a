4eaa7117db203e7545d72955fa8d3ff0762ad95f
nairx <nairxprv@gmail.com>
2025-07-01 10:08:38 +0530

    updated

--- mongodb/q11.js
+++ mongodb/q11.js
@@ -11,3 +11,13 @@ session.commitTransaction()
 session.endSession()
 db.customers.find()
 exit
+///////////////////////////////////////
+mongosh "mongodb://localhost:27018,localhost:27019,localhost:27020/hdfc?replicaSet=rs1"
+const session = db.getMongo().startSession();
+session.startTransaction()
+var custCollection = session.getDatabase("hdfc").customers
+custCollection.updateOne({_id:1},{$inc:{bal:-100}})
+// don't run this - custCollection.updateOne({_id:2},{$inc:{bal:100}})
+session.abortTransaction() 
+session.endSession()
+db.customers.find()
