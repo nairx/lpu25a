04ed2a50b1ad1cad24e4148aac6c5b1576a60fed
nairx <nairxprv@gmail.com>
2025-07-04 11:52:40 +0530

    UPdated

--- nodejs/nodeapp/index11.js
+++ nodejs/nodeapp/index11.js
@@ -1,22 +1,12 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const SECRET = "secret";
 const app = express();
 app.listen(8080, () => {
   console.log("Server started");
 });
-const users = [
-  {
-    email: "john@gmail.com",
-    pass: "1234",
-    role: "user",
-  },
-  {
-    email: "cathy@gmail.com",
-    pass: "1234",
-    role: "admin",
-  },
-];
+let users = [];
 app.use(express.json());
 
 const authenticate = (req, res, next) => {
@@ -32,15 +22,20 @@ const authenticate = (req, res, next) => {
 };
 
 const authorize = (role) => {
-  return (req, res, next) => {};
+  return (req, res, next) => {
+    if (req.role === "admin") {
+      next();
+    } else {
+      return res.status(403).json({ message: "Unauthorized Access" });
+    }
+  };
 };
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, pass } = req.body;
-  const found = users.find(
-    (user) => user.email === email && user.pass === pass
-  );
-  if (found) {
+  const existingUser = users.find((user) => user.email === email);
+  const matchPass = await bcrypt.compare(pass, existingUser.pass);
+  if (matchPass) {
     const token = jwt.sign(found, SECRET, { expiresIn: "1h" });
     res.status(200).json({ user: found, token });
   } else {
@@ -48,6 +43,19 @@ app.post("/login", (req, res) => {
   }
 });
 
-app.get("/users", authenticate, authorize(req.role), (req, res) => {
+app.post("/register", async (req, res) => {
+  const { name, email, pass, role } = req.body;
+  const hashedpwd = await bcrypt.hash(pass, SECRET);
+  const user = {
+    name,
+    email,
+    pass: hashedpwd,
+    role,
+  };
+  users.push(user);
+  res.json(user);
+});
+
+app.get("/users", authenticate, authorize("admin"), (req, res) => {
   res.json(users);
 });
