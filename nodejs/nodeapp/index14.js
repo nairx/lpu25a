eec7ca376a707c49951f2d748cd623a99f031ed3
nairx <nairxprv@gmail.com>
2025-07-05 11:49:39 +0530

    updated

--- nodejs/nodeapp/index14.js
+++ nodejs/nodeapp/index14.js
@@ -22,6 +22,28 @@ const userSchema = mongoose.Schema(
 );
 const userModel = mongoose.model("User", userSchema);
 
+const authenticate = (req, res, next) => {
+  try {
+    let token = req.headers.authorization;
+    token = token.split(" ")[1];
+    const user = jwt.verify(token, SECRET);
+    req.role = user.role;
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.json({ message: "Invalid Token" });
+  }
+};
+
+const authorize = (role) => {
+  return (req, res, next) => {
+    if (role === req.role) {
+      next();
+    } else {
+      return res.json({ message: "Unauthorized access" });
+    }
+  };
+};
 app.use(express.json());
 app.post("/register", async (req, res) => {
   try {
