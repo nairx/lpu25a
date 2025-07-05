dcca89d781285dd3006ecfe9fa4c6bf567f4e4f6
nairx <nairxprv@gmail.com>
2025-07-05 10:26:53 +0530

    updated

--- nodejs/nodeapp/index14.js
+++ nodejs/nodeapp/index14.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import express from "express";
 const app = express();
-
 mongoose.connect("mongodb://localhost:27017/lpu").then(() => {
   app.listen(8080, () => {
     console.log("Server started");
@@ -13,9 +12,27 @@ const userSchema = mongoose.Schema({
   name: { type: String },
   email: { type: String, unique: true },
   password: { type: String },
+  role: { type: String },
 });
 const userModel = mongoose.model("User", userSchema);
 
 app.get("/users", (req, res) => {
   res.json({});
+});
+app.use(express.json());
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const user = {
+      name,
+      email,
+      password,
+      role,
+    };
+    const result = await userModel.create(user);
+    res.status(201).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
 });
