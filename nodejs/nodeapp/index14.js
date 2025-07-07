9b8515de8572e6c5a9fad08d596c48642316eeb9
nairx <nairxprv@gmail.com>
2025-07-07 11:18:42 +0530

    updated

--- nodejs/nodeapp/index14.js
+++ nodejs/nodeapp/index14.js
@@ -1,15 +1,11 @@
 import mongoose from "mongoose";
 import express from "express";
 import userRouter from "./userRoute.js"
-
 const app = express();
-
 mongoose.connect("mongodb://localhost:27017/lpu").then(() => {
   app.listen(8080, () => {
     console.log("Server started");
   });
 });
-
 app.use(express.json());
-
 app.use("/api/users", userRouter);
